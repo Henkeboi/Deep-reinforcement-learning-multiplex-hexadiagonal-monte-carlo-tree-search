4f530a90df8755d5ac4fd8b65944d4a4fe395120
Henrik <hgg1998@gmail.com>
2021-03-29 18:00:45 +0200

    Add crossentropy loss.

--- neural_actor.py
+++ neural_actor.py
@@ -5,6 +5,7 @@ import copy
 import operator
 import torch
 import numpy as np
+from torch.autograd import Variable
 
 class Network(torch.nn.Module):
     def __init__(self, hidden_layers, output_size):
@@ -27,41 +28,48 @@ class NeuralActor:
         self.num_max_moves = num_max_moves
         self.nn = Network(hidden_layers, num_max_moves)
         self.learning_rate = learning_rate
-        #self.loss_function = torch.nn.CrossEntropyLoss()
-        self.loss_function = torch.nn.MSELoss()
+        self.loss_function = torch.nn.CrossEntropyLoss()
+        #self.loss_function = torch.nn.MSELoss()
         self.optimizer = torch.optim.SGD(self.nn.parameters(), lr=self.learning_rate)
    
     def update_Q(self, training_data):
-        state, label = training_data[0]
+        for i in range(0, len(training_data)):
+            state, label = training_data[i]
+            state  = np.asarray(state, dtype=float)
+            state = torch.tensor(state, requires_grad=True)
+            state = state.view(1, state.shape[0])
+            max_val = max(label)
+            index = list.index(label, max_val)
+            label = torch.from_numpy(np.asarray([index]))
+            nn_loss = self.loss_function(state, label)
+            nn_loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+    def get_action(self, state):
         state = torch.from_numpy(state).float()
         label = torch.from_numpy(np.asarray(label)).float()
         label = torch.nn.functional.softmax(label, dim=0).float()
-        nn_output = self.nn(state) # Forward pass
-        nn_output = nn_output.view(3, 1)
-        nn_loss = self.loss_function(nn_output, label)
-        nn_loss.backward() 
-        self.optimizer.step() 
-        self.optimizer.zero_grad()
-    
 
 def main():
     max_removable_pieces = 3
-    hidden_layers = [3, 4, 3] 
-    la = 0.4
+    hidden_layers = [3, 20, 30, 3] 
+    la = 0.1
     neural_actor = NeuralActor(hidden_layers, max_removable_pieces, la)
 
     number_of_pieces = 40
     state_manager = StateManager(number_of_pieces, max_removable_pieces)
     num_search_games = 1
-    num_simulations = 50
+    num_simulations = 100
     max_depth = 20
-    mct = MCT(state_manager, num_search_games, num_simulations, max_depth)
+    mct = MCT(num_search_games, num_simulations, max_depth)
 
-    mct.play_game()
-    training_data = mct.get_training_data()
-    neural_actor.update_Q(training_data)
+    for i in range(0, 100):
+        mct.play_game(copy.deepcopy(state_manager))
+        training_data = mct.get_training_data()
+        neural_actor.update_Q(training_data)
 
-    #mct.play_game()
+    #neural_actor.get_action(state_manager.
 
 
 if __name__ == '__main__':
